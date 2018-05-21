5cc882dbf20c54c3f40e767079f658ae00e6ee9d
abril <abyahernandezsa@ittepic.edu.mx>
2018-05-21 08:32:58 -0600

    primer ejercicio

---
+++ eje01.js
@@ -0,0 +1,9 @@
+var nombre;
+nombre = "Abril";
+var edad = 22;
+var persona = {
+    nombre: nombre,
+    edad: edad
+};
+var mensaje = "Mi nombre es " + persona.nombre + " y tengo " + persona.edad + "a\u00F1os";
+console.log(mensaje);
---
+++ eje01.ts
@@ -0,0 +1,12 @@
+let nombre:string;
+nombre="Abril";
+let edad:number=22;
+
+let persona = {
+    nombre:nombre,
+    edad:edad
+}
+
+let mensaje:string = `Mi nombre es ${persona.nombre} y tengo ${persona.edad}años`;
+
+console.log(mensaje);
